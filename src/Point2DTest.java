dfe6b870bfa64f61709207f3be46811c9f2f499a
Tuan le <leminhtuan48198@gmail.com>
2022-08-11 13:26:27 +0700

    add file point 2D 3D

---
+++ src/Point2DTest.java
@@ -0,0 +1,10 @@
+public class Point2DTest {
+    public static void main(String[] args) {
+        Point2D point2D=new Point2D();
+        System.out.println(point2D);
+        point2D=new Point2D(1,1);
+        System.out.println(point2D);
+    }
+
+
+}
---
+++ src/Point3DTest.java
@@ -0,0 +1,8 @@
+public class Point3DTest {
+    public static void main(String[] args) {
+        Point3D p3D=new Point3D();
+        System.out.println(p3D);
+        p3D=new Point3D(2,3,4);
+        System.out.println(p3D);
+    }
+}
